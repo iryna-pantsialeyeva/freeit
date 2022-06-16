b5d071be55f627d8d0464c15bc3053ce62b39460
iryna-pantsialeyeva <qatirina7@gmail.com>
2022-06-16 20:23:37 +0400

    initial commit

---
+++ src/task2/Main.java
@@ -0,0 +1,17 @@
+package task2;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 19;
+        if (a < b) {
+            System.out.println("число a меньше числа b");
+        }
+        else if (a > b) {
+            System.out.println("число а больше числа b");
+        }
+        else {
+            System.out.println("числа a и b равны");
+        }
+    }
+}
