6bf359ace462cf97b79064582b6241bf59790734
iryna-pantsialeyeva <qatirina7@gmail.com>
2022-07-03 17:15:45 +0400

    add update of SumDivisibleBy3Number by request in PR added code fits for the sum of first n natural numbers

--- src/task5/SumDivisibleBy3Number.java
+++ src/task5/SumDivisibleBy3Number.java
@@ -4,6 +4,7 @@ package task5;
 NB Поскольку в задаче не указано, от какого числа вести отсчет, а отрицательные числа также являются целыми числами,
 задача решена для следующего условия (чтобы можно было вести отсчет от любого введенного целого числа):
  "Найдите сумму первых n целых чисел, которые делятся на 3. Отсчет вести от числа n, введенного с консоли".
+ По запросу добавлен второй варинат решения - для отсчета для первых натуральных чисел (от 1).
  */
 
 import java.util.Scanner;
@@ -21,7 +22,7 @@ public class SumDivisibleBy3Number {
         }
         int counter = inputInteger;
 
-
+        // 1. отсчет ведется от числа inputInteger (оно же счетчик слов)
         while (inputInteger % 3 != 0) {
             inputInteger += 1;
         }
@@ -34,6 +35,24 @@ public class SumDivisibleBy3Number {
             sum += addNumber;
         }
 
-        System.out.println("Sum of the first of " + counter + " numbers divisible by 3 is equal to " + sum + ".");
+        System.out.println("Sum of the first of " + counter + " numbers divisible by 3 is equal to " + sum +
+                " (sum counted from entered number).");
+
+        // 2. отсчет ведется от 1
+        inputInteger = 1;
+        while (inputInteger % 3 != 0) {
+            inputInteger += 1;
+        }
+
+        sum = inputInteger;
+        addNumber = inputInteger;
+
+        for (int i = 1; i < counter; i++) {
+            addNumber += 3;
+            sum += addNumber;
+        }
+
+        System.out.println("Sum of the first of " + counter + " natural numbers divisible by 3 is equal to " + sum +
+                ".");
     }
 }
