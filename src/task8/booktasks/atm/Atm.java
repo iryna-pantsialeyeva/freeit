87bb0de4058f3404e6c8c5e206af54d8b3043e1e
iryna-pantsialeyeva <qatirina7@gmail.com>
2022-07-18 00:15:44 +0400

    add task8.Part2: Classes and Objects tasks from book Part 2 p.25

---
+++ src/task8/booktasks/atm/Atm.java
@@ -0,0 +1,78 @@
+package task8.booktasks.atm;
+
+/* Задание 25 (Book, Part 2, P. 25). Создать класс и объекты описывающие Банкомат. Набор купюр находящихся в
+                                     банкомате должен задаваться тремя свойствами: количеством купюр номиналом 20
+                                     50 100. Сделать методы для добавления денег в банкомат. Сделать функцию
+                                     снимающую деньги. На вход передается сумма денег. На выход – булевское
+                                     значение (операция удалась или нет). При снятии денег функция должна
+                                     рапечатывать каким количеством купюр какого номинала выдается сумма. Создать
+                                     конструктор с тремя параметрами – количеством купюр.*/
+public class Atm {
+
+    private int usd100;
+    private int usd50;
+    private int usd20;
+
+    public Atm(int usd100, int usd50, int usd20) {
+        this.usd20 = usd20;
+        this.usd50 = usd50;
+        this.usd100 = usd100;
+    }
+
+    public int getUsd20() {
+        return usd20;
+    }
+
+    public int getUsd50() {
+        return usd50;
+    }
+
+    public int getUsd100() {
+        return usd100;
+    }
+
+    public void setUsd100(int usd100) {
+        this.usd100 = usd100;
+    }
+
+    public void setUsd50(int usd50) {
+        this.usd50 = usd50;
+    }
+
+    public void setUsd20(int usd20) {
+        this.usd20 = usd20;
+    }
+
+    public int getAmountInAtm() {
+        return getUsd100() * 100 + getUsd50() * 50 + getUsd20() * 20;
+    }
+
+    public int topUpCount(int usd100, int usd50, int usd20) {
+        setUsd100(getUsd100() + usd100);
+        setUsd50(getUsd50() + usd50);
+        setUsd20(getUsd20() + usd20);
+        return getAmountInAtm();
+    }
+
+
+    public boolean isEnoughForWithdraw(int neededMoney) {
+        if (getAmountInAtm() >= neededMoney) {
+            this.usd100 -= neededMoney / 100;
+            this.usd50 -= (neededMoney % 100) / 50;
+            this.usd20 -= ((neededMoney % 100) % 50) / 20;
+            return true;
+        }
+        return false;
+    }
+
+    public void print(int neededMoney) {
+        if (!isEnoughForWithdraw(neededMoney)) {
+            System.out.println("Non-sufficient funds.\nTry withdrawing money from another ATM.");
+        } else {
+            System.out.println("Withdrawing money from an ATM ...Please wait: \nGet a transaction receipt.");
+            System.out.print("USD100 banknote(s): " + neededMoney / 100 +
+                    "\nUSD50 banknote(s): " + ((neededMoney % 100) / 50) +
+                    "\nUSD20 banknote(s): " + (((neededMoney % 100) % 50) / 20));
+        }
+    }
+}
---
+++ src/task8/booktasks/timeline/Timeline.java
@@ -0,0 +1,58 @@
+package task8.booktasks.timeline;
+
+/* Задание 24 (Book, Part 2, P. 25). Создать класс и объекты описывающие промежуток времени. Сам промежуток в классе
+                                     должен задаваться тремя свойствами: секундами, минутами, часами.
+                                     Сделать методы для получения полного количества секунд в объекте, сравнения
+                                     двух объектов (метод должен работать аналогично compareTo в строках). Создать
+                                     два конструктора: получающий общее количество секунд, и часы, минуты и секунды
+                                     по отдельности. Сделать метод для вывода данных.  */
+
+public class Timeline {
+
+    private int hour;
+    private int min;
+    private int sec;
+
+    public Timeline(int hour, int min, int sec) {
+        this.hour = hour;
+        this.min = min;
+        this.sec = sec;
+    }
+
+    public Timeline(int sec) {
+        this.sec = sec;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public int getSec() {
+        return sec;
+    }
+
+    public int calculateSeconds() {
+        return (((this.hour * 60) + this.min) * 60 + this.sec);
+    }
+
+    public int compareTo(Timeline timeline) {
+        int index = this.calculateSeconds() - timeline.calculateSeconds();
+        return index;
+    }
+
+    public static void print(int index) {
+        if (index > 0) {
+            System.out.println("timeline1 > timeline2");
+        } else if (index < 0) {
+            System.out.println("timeline1 < timeline2");
+        } else {
+            System.out.println("timeline1 = timeline2");
+        }
+    }
+}
+
+
